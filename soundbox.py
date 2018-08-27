4718b10a3b16c66395fa5b83d090a0d912396354
nathandarnell <nathanrdarnell@gmail.com>
2018-08-26 19:53:13 -0700

    Added in GPIO pin 11

--- soundbox.py
+++ soundbox.py
@@ -5,7 +5,7 @@ import os
 import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BOARD) # https://sourceforge.net/p/raspberry-gpio-python/wiki/BasicUsage/
-chan_list = [7]    # add as many channels as you want!
+chan_list = [7,11]    # add as many channels as you want!
                        # you can tuples instead i.e.:
                        #   chan_list = (11,12)
 GPIO.setup(chan_list, GPIO.IN, pull_up_down=GPIO.PUD_UP) # https://sourceforge.net/p/raspberry-gpio-python/wiki/Inputs/
