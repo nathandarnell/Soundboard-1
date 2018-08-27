bf83ec31972daf14035d10d4a9e3301ace6f6146
nathandarnell <nathanrdarnell@gmail.com>
2018-08-27 11:04:41 -0700

    Added in pins to make 8 total

--- soundbox.py
+++ soundbox.py
@@ -7,7 +7,7 @@ import RPi.GPIO as GPIO
 # https://sourceforge.net/p/raspberry-gpio-python/wiki/BasicUsage/
 # https://raspi.tv/2014/rpi-gpio-update-and-detecting-both-rising-and-falling-edges
 GPIO.setmode(GPIO.BOARD)
-gpio_list = [7,11]    # Pins to poll https://pinout.xyz/pinout/pin11_gpio17
+gpio_list = [7,11,13,15,29,31,33,37]    # Pins to poll https://pinout.xyz/pinout/pin11_gpio17
 GPIO.setup(gpio_list, GPIO.IN, pull_up_down=GPIO.PUD_UP) # https://sourceforge.net/p/raspberry-gpio-python/wiki/Inputs/
 
 dir_path = os.path.dirname(os.path.realpath(__file__)) #https://stackoverflow.com/questions/5137497/find-current-directory-and-files-directory
