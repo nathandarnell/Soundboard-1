55a30eb45152c7ef4b90778b6efa9c1917e1f38b
nathandarnell <nathanrdarnell@gmail.com>
2018-08-26 19:51:12 -0700

    Update soundbox.py

--- soundbox.py
+++ soundbox.py
@@ -17,10 +17,10 @@ pygame.init()
 pygame.mixer.init()
 
 def playSound(path):
-#    pygame.mixer.music.stop()
-#    pygame.mixer.music.load(path)
+    pygame.mixer.music.stop()
+    pygame.mixer.music.load(path)
     print(path)
-#    pygame.mixer.music.play()
+    pygame.mixer.music.play()
 
 try:
 # https://sourceforge.net/p/raspberry-gpio-python/wiki/Inputs/
