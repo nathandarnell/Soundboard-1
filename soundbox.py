d2b71d9cdeee69dfdcb612369f7ec5babc77e88c
nathandarnell <nathanrdarnell@gmail.com>
2018-08-26 22:22:55 -0700

    Update soundbox.py

--- soundbox.py
+++ soundbox.py
@@ -3,18 +3,25 @@ import time
 import pygame
 import os
 import RPi.GPIO as GPIO
+import glob
 
-GPIO.setmode(GPIO.BOARD) # https://sourceforge.net/p/raspberry-gpio-python/wiki/BasicUsage/
+# https://sourceforge.net/p/raspberry-gpio-python/wiki/BasicUsage/
+# https://raspi.tv/2014/rpi-gpio-update-and-detecting-both-rising-and-falling-edges
+GPIO.setmode(GPIO.BOARD)
 gpio_list = [7,11]    # Pins to poll https://pinout.xyz/pinout/pin11_gpio17
 GPIO.setup(gpio_list, GPIO.IN, pull_up_down=GPIO.PUD_UP) # https://sourceforge.net/p/raspberry-gpio-python/wiki/Inputs/
+
+dir_path = os.path.dirname(os.path.realpath(__file__)) #https://stackoverflow.com/questions/5137497/find-current-directory-and-files-directory
 
 pygame.init()
 pygame.mixer.init()
 
+
 def getPath(pin):
     pin = str(pin) # https://www.crybit.com/convert-integer-string-python/
-    pin += '.wav'
+    pin += '.mp3'
     playSound(pin)
+
 
 def playSound(path):
     pygame.mixer.music.stop()
